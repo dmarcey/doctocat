dccd226dc96336c6b49a2fb25f929db2856a3701
Cole Bemis <colebemis@github.com>
2019-08-07 14:00:39 -0700

    Add path prefix

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     title: 'Doctocat',
     description: 'A Gatsby theme for creating Primer documentation sites',
   },
+  pathPrefix: '/doctocat',
   __experimentalThemes: [
     {
       resolve: '@primer/gatsby-theme-doctocat',
